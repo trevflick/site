02413f225f1d8c0f9582383bf6f6d5add83179da
Heyguys101 <trev.flick@gmail.com>
2025-01-19 13:18:26 -0700

    walking animations added

--- main.js
+++ main.js
@@ -17,17 +17,57 @@ const config = {
   // preload room and sprite
   function preload() {
 
-    this.load.image('floor', 'assets/floor.png');
+    this.load.image('scene', 'assets/scene.png');
+    
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 
+        'assets/dude.png',
+        { frameWidth: 32, frameHeight: 48 });
+
+    this.load.spritesheet('idleLeft', 'assets/idleLeft.png', {frameWidth:64, frameHeight:64})
+    this.load.spritesheet('idleRight', 'assets/idleRight.png', {frameWidth:64, frameHeight:64})
+    
+
 
   }
   
   // initialize objects
   function create() {
-    // char
-    // background
-    this.add.image(0, 0, 'floor').setOrigin(0, 0);
+    // the order you add items is how they appear (make sure background is before everything else)
 
-    
+    // background
+    this.add.image(0, 0, 'scene').setOrigin(0, 0);
+
+    // char (might need to figure this out, idk if i need two players for left and right yet)
+    player = this.physics.add.sprite(100, 450, 'idleLeft');
+
+    // animations (i made different sprites for each movement, instead of all together)
+    //need animation for standing still...so no key input
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('walkLeft', { start: 0, end: 4 }),
+      frameRate: 7,
+      repeat: -1
+    });
+
+    // need animation for walking left
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('walkingLeft', { start: 0, end: 4 }),
+      frameRate: 7,
+      repeat: -1
+    });
+
+    // walking right
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('walkingRight', { start: 0, end: 4 }),
+      frameRate: 7,
+      repeat: -1
+    });
+
   }
   
   // gonna get called for every frame update (idk what phaser framerate is auto set to)
